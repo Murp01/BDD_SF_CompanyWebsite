335a1ec8d4a5b7c2e42bf281f405daed22e7cea9
Paul.Murphy <Paul.Murphy@ROQ-028>
2019-09-16 11:41:23 +0100

    Report set up but I couldn't get it working.  I need to look at a different tutorial

--- CompanyWebsitePageFactory/Runner/CucumberTestRunner.cs
+++ CompanyWebsitePageFactory/Runner/CucumberTestRunner.cs
@@ -1,15 +1,34 @@
 ﻿using CompanyWebsitePageFactory.BrowserWrapper;
 using System.Configuration;
 using TechTalk.SpecFlow;
+using RelevantCodes.ExtentReports; //reporting addition
+using CompanyWebsitePageFactory.Reports; //reporting addition
+
 
 namespace CompanyWebsitePageFactory.Runner
 {
     [Binding]
     public class CucumberTestRunner
     {
+        private static ReportingTasks _reportingTasks; //reporting addition
+
         [Before]
         public void ScenarioSetup()
         {
+
+            ExtentReports extentReports = ReportingManager.Instance;
+
+            //extentReports.LoadConfig(Directory.GetParent(TestContext.CurrentContext.TestDirectory).Parent.FullName + "\\extent-config.xml");
+
+            //Note we have hardcoded the browser, we will deal with this later
+
+            extentReports.AddSystemInfo("Browser", "Chrome");
+
+
+
+            _reportingTasks = new ReportingTasks(extentReports);
+
+
             BrowserFactory.InitBrowser("Chrome");
             BrowserFactory.Driver.Manage().Window.Maximize();
             BrowserFactory.GoToURL(ConfigurationManager.AppSettings["URL"]);
@@ -18,6 +37,7 @@ namespace CompanyWebsitePageFactory.Runner
         [After]
         public void ScenarioTeardown()
         {
+            _reportingTasks.FinalizeTest(); //reporting addition
             BrowserFactory.CloseAllDrivers();
         }
     }
