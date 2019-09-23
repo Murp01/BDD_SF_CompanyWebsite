f0eeeea35b69f8f89e468b1694ea56e875b98434
PaulWork <pmurphy35@hotmail.com>
2019-09-23 14:28:54 +0100

    Added BBC Website example.  This is just one feature test which opens bbc website > Clicks on More button.
    Next step is to remove the LL features

---
+++ CompanyWebsitePageFactory/Features/PrimaryNavigationMenu.feature
@@ -0,0 +1,13 @@
+﻿Feature: PrimaryNavigationMenu
+	The Primary Navigation Menu is the main banner at the
+	top of the webpage.  The menu contains the Homepage button,
+	Sign in button, search box and page navigation buttons.
+	Note: search box and Sign in have their own feature files
+
+Background: Test begins on site homepage
+	Given I am on the BBC homepage
+
+@Ready
+Scenario: More drop down reveals further nav options	
+	When I click on the More drop down button
+	Then further nav options are revealed below the primary nav bar
---
+++ CompanyWebsitePageFactory/StepDefinitions/BBCHomeSteps.cs
@@ -0,0 +1,30 @@
+﻿using CompanyWebsitePageFactory.BrowserWrapper;
+using CompanyWebsitePageFactory.Extensions;
+using CompanyWebsitePageFactory.PageObjects;
+using System.Configuration;
+using TechTalk.SpecFlow;
+
+namespace CompanyWebsitePageFactory.StepDefinitions
+{
+    [Binding]
+    public class PrimaryNavigationMenuSteps
+    {
+        [Given(@"I am on the BBC homepage")]
+        public void GivenIAmOnTheBBCHomepage()
+        {
+            BrowserFactory.GoToURL(ConfigurationManager.AppSettings["URL"]);
+        }
+        
+        [When(@"I click on the More drop down button")]
+        public void WhenIClickOnTheMoreDropDownButton()
+        {
+            PageObjectFactory.BBCHomePage.ClickOnMoreBtn();
+        }
+        
+        [Then(@"further nav options are revealed below the primary nav bar")]
+        public void ThenFurtherNavOptionsAreRevealedBelowThePrimaryNavBar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
